58d294435056bd981b027ac23b5bdefb924ec844
escrocket <rocket@rockets.chat>
2023-08-24 00:58:42 +0100

    Fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            runtime: 'nodejs18.x'
+        }),
     },
 
     preprocess: [
