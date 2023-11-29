bc33be1ab66220b68fb36a3847e65bc360b1293e
beanznotavailable <59755055+beanznotavailable@users.noreply.github.com>
2023-11-29 08:32:20 +0545

    feat: add pagination option to offers api

--- src/routes/api/offers/+server.js
+++ src/routes/api/offers/+server.js
@@ -1,19 +1,38 @@
 import response from "$lib/response";
 import Offer from "../../../models/Offer.model";
 
-export const GET = async(request) => {
-    const { country } = JSON.parse(process.env.geoInfo)
+export const GET = async (request) => {
+  const { country } = { country: "US" } || JSON.parse(process.env.geoInfo);
 
-    const latestOffer = await Offer.find().sort({ _id: -1 }).limit(1);
-    const v = latestOffer[0].v;
+  const latestOffer = await Offer.find().sort({ v: -1 }).limit(1);
+  const v = latestOffer[0].v;
 
-    var search = { country, v };
-    const param = request.url.searchParams.get("categoryName");
-    if (param !== null) {
-        search["category_name_readable"] = param;
-    }
+  var search = { country, v };
+  const param = request.url.searchParams.get("categoryName");
+  const page = Number(request.url.searchParams.get("page"));
+  const limit = Number(request.url.searchParams.get("limit"));
 
-    const offers = await Offer.find(search).select({
+  if (param) search["category_name"] = param;
+
+  // console.log(search, "param");
+
+  // if (param?.includes(",")) {
+  //   // console.log("array");
+  //   // if array is sent from client
+  //   search["category_name_readable"] = { $in: param?.split(",") };
+  // } else if (param !== null) {
+  //   search["category_name_readable"] = param;
+  // }
+
+  console.log(page, limit, "page ,limit");
+
+  let offers;
+
+  if (page && limit) {
+    offers = await Offer.paginate(search, {
+      limit: limit || 50,
+      page: page || 1,
+      select: {
         title: 1,
         description: 1,
         link: 1,
@@ -21,11 +40,33 @@ export const GET = async(request) => {
         creative: 1,
         mobile_app_type: 1,
         conversion: 1,
-        v: 1
+        v: 1,
+        category_name_readable: 1,
+      },
+      sort: {
+        amount: -1,
+      },
     });
+  } else {
+    offers = await Offer.find(search)
+      .select({
+        title: 1,
+        description: 1,
+        link: 1,
+        tokens: 1,
+        creative: 1,
+        mobile_app_type: 1,
+        conversion: 1,
+        v: 1,
+        category_name_readable: 1,
+      })
+      .sort({ amount: -1 });
+  }
 
-    return response({
-        success: true,
-        offers
-    });
-}
+  // console.log(offers, "ffers");
+
+  return response({
+    success: true,
+    offers,
+  });
+};
