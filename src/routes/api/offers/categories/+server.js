8086875d35e037e6b028f43c80b62efb2fabc940
beanznotavailable <59755055+beanznotavailable@users.noreply.github.com>
2023-11-29 08:33:31 +0545

    feat: use aggregation instead

--- src/routes/api/offers/categories/+server.js
+++ src/routes/api/offers/categories/+server.js
@@ -3,35 +3,62 @@ import Offer from "../../../../models/Offer.model";
 import response from "$lib/response";
 
 // TODO: Remove "US" hard code
-export const GET = async(request) => {
-    const { country } = JSON.parse(process.env.geoInfo)
+export const GET = async (request) => {
+  const { country } = { country: "US" } || JSON.parse(process.env.geoInfo);
+  // const { country } = JSON.parse(process.env.geoInfo);
 
-    const latestOffer = await Offer.find().sort({ _id: -1 }).limit(1);
-    const v = latestOffer[0].v;
+  // const latestOffer = await Offer.find().sort({ _id: -1 }).limit(1);
+  const latestOffer = await Offer.find().sort({ v: -1 }).limit(1);
+  // console.log(latestOffer);
+  const v = latestOffer[0].v;
 
-    const offers = await Offer.find({ v }).distinct("category_name_readable");
+  // const offers = await Offer.find({ v }).distinct("category_name_readable");
 
-    var categories = [];
-    var total = 0;
-    for (var i = 0; i < offers.length; i++) {
-        const offer = offers[i];
-        const count = await Offer.countDocuments({
-            category_name_readable: offer,
-            country,
-            v
-        });
+  let categories = await Offer.aggregate([
+    {
+      $match: {
+        country,
+        v,
+      },
+    },
+    {
+      $group: {
+        _id: "$category_name_readable",
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: "$_id",
+        count: 1,
+      },
+    },
+  ]);
 
-        categories.push({
-            name: offer,
-            count
-        });
+  // console.log(offers.length);
 
-        total += count;
-    }
+  // var categories = [];
+  // var total = 0;
+  // for (var i = 0; i < offers.length; i++) {
+  //   const offer = offers[i];
+  //   const count = await Offer.countDocuments({
+  //     category_name_readable: offer,
+  //     country,
+  //     v,
+  //   });
 
-    return response({
-        success: true,
-        categories,
-        total
-    });
-}
+  //   categories.push({
+  //     name: offer,
+  //     count,
+  //   });
+
+  //   total += count;
+  // }
+
+  return response({
+    success: true,
+    categories,
+    total: categories.reduce((acc, curr) => acc + curr.count, 0),
+  });
+};
