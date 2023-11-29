04e2e436bdcd5b210fd207213ba25b7fbde5447d
beanznotavailable <59755055+beanznotavailable@users.noreply.github.com>
2023-11-29 08:28:44 +0545

    update offers and offers type page to use pagination
    paginaiton on scroll
    update values in offer.store

--- src/routes/offers/+page.svelte
+++ src/routes/offers/+page.svelte
@@ -1,68 +1,147 @@
 <script>
-    import { onMount } from "svelte";
-    import { get } from "svelte/store";
-    import EarnHeader from "../../components/EarnHeader.svelte";
-    import Offer from "../../components/Offer.svelte";
-    import OfferPage from "../../components/OfferPage.svelte";
-    import tokenStore from "../../stores/token.store";
+  import { onMount } from "svelte";
+  import { get } from "svelte/store";
+  import EarnHeader from "../../components/EarnHeader.svelte";
+  import Offer from "../../components/Offer.svelte";
+  import OfferPage from "../../components/OfferPage.svelte";
+  import tokenStore from "../../stores/token.store";
+  import LoadingSpinner from "$components/common/Loading/LoadingSpinner.svelte";
 
-    var header;
-    var userId = "";
+  let devices = {
+    android: true,
+    apple: true,
+    computer: true,
+  };
+  let userId = "";
 
-    var offers = [];
-    var filteredOffers = [];
-    const getOffers = async() => {
-        const response = await fetch("/api/offers");
-        const data = await response.json();
+  let offers = [];
+  let filteredOffers = [];
+  let apiResponse;
 
-        if (!data.success) {
-            // TODO: handle error
-            return;
-        }
+  // let offerLists = [];
+  let page = 1;
+  let limit = 40;
 
-        offers = data.offers;
-        filterOffers();
+  let loading = false;
+
+  let offerPageNode;
+
+  const getOffers = async (page, limit) => {
+    loading = true;
+    const response = await fetch(`/api/offers?page=${page}&limit=${limit}`);
+    const data = await response.json();
+
+    apiResponse = data;
+
+    if (!data.success) {
+      // TODO: handle error
+      return;
     }
 
-    const filterOffers = () => filteredOffers = offers.filter(offer => {
-        if (header.apple && (offer.mobile_app_type === "ios" || offer.mobile_app_type === null)) {
-            return true;
-        }
+    offers = [...offers, ...data.offers.docs];
+    filterOffers(devices);
+    // console.log(offerPageNode?.scrollHeight);
+    loading = false;
+  };
 
-        if (header.android && (offer.mobile_app_type === "android" || offer.mobile_app_type === null)) {
-            return true;
-        }
+  const filterOffers = (devices) =>
+    (filteredOffers = offers.filter((offer) => {
+      if (
+        devices.apple &&
+        (offer.mobile_app_type === "ios" || offer.mobile_app_type === null)
+      ) {
+        return true;
+      }
 
-        if (header.computer && (offer.mobile_app_type === null)) {
-            return true;
-        }
+      if (
+        devices.android &&
+        (offer.mobile_app_type === "android" || offer.mobile_app_type === null)
+      ) {
+        return true;
+      }
 
-        return false;
+      if (devices.computer && offer.mobile_app_type === null) {
+        return true;
+      }
+
+      return false;
+    }));
+
+  onMount(() => {
+    const token = get(tokenStore);
+    if (token !== "") {
+      const rawData = JSON.parse(atob(token.split(".")[1]));
+      userId = rawData["uid"];
+    }
+
+    getOffers(page, limit);
+
+    // to listen to scroll and call api afer user starts reaching end
+    window.addEventListener("scroll", () => {
+      if (
+        offerPageNode?.scrollHeight - window?.scrollY < 500 &&
+        apiResponse?.offers?.hasNextPage &&
+        loading === false
+      ) {
+        page = page + 1;
+        getOffers(page, limit);
+        // console.log("hit api now", page);
+      }
+      // console.log(window?.scrollY, offerPageNode?.scrollHeight);
     });
+  });
 
-    onMount(() => {
-        const token = get(tokenStore);
-        if (token !== "") {
-            const rawData = JSON.parse(atob(token.split(".")[1]));
-            userId = rawData["uid"];
-        }
+  $: filterOffers(devices);
 
-        getOffers();
-    });
+  // $: offerLists = [
+  //   ...offerLists,
+  //   ...filteredOffers.slice(size * page, size * (page + 1) - 1),
+  // ];
+
+  // $: console.log(offers, filteredOffers, "page");
+  // $: console.log(offerPageNode?.scrollHeight);
 </script>
 
 <svelte:head>
-    <title>Offers - Justearn.com</title>
+  <title>Offers - Justearn.com</title>
 </svelte:head>
 
-<EarnHeader name="Offers" onChange={filterOffers} bind:this={header}/>
+<EarnHeader name="Offers" onChange={filterOffers} bind:devices />
 
-
-<OfferPage>
+<div style="" bind:this={offerPageNode}>
+  <OfferPage>
     {#each filteredOffers as offer}
-        <Offer dollars={Math.floor(offer.tokens / 100)} cents={parseInt(offer.tokens % 100)} offerImage={offer.creative} title={offer.title} description={offer.description} type="OTHER"
-            android={(offer.mobile_app_type === "android" || offer.mobile_app_type === null)}
-            apple={(offer.mobile_app_type === "ios" || offer.mobile_app_type === null)}
-            computer={(offer.mobile_app_type === null)} offerUrl={offer.link.replace("[USERIDHERE]", userId)}/>
+      <Offer
+        currencyAward={offer.tokens}
+        offerImage={offer.creative}
+        title={offer.title}
+        description={offer.description}
+        type={offer.category_name_readable}
+        android={offer.mobile_app_type === "android" ||
+          offer.mobile_app_type === null}
+        apple={offer.mobile_app_type === "ios" ||
+          offer.mobile_app_type === null}
+        computer={offer.mobile_app_type === null}
+        offerUrl={offer.link.replace("[USERIDHERE]", userId)}
+      />
     {/each}
-</OfferPage>
+  </OfferPage>
+</div>
+
+{#if loading}
+  <div class="spinner-container">
+    <LoadingSpinner />
+  </div>
+{/if}
+
+<style lang="scss">
+  .spinner-container {
+    display: grid;
+    place-items: center;
+    height: 100px;
+    width: 100%;
+    background: #171515;
+
+    // background: red;
+  }
+</style>
--- src/routes/offers/[type]/+page.svelte
+++ src/routes/offers/[type]/+page.svelte
@@ -1,67 +1,220 @@
 <script>
-    import { onMount } from "svelte";
-	import { get } from "svelte/store";
-    import EarnHeader from "../../../components/EarnHeader.svelte";
-    import Offer from "../../../components/Offer.svelte";
-    import OfferPage from "../../../components/OfferPage.svelte";
-	
+  import { onMount } from "svelte";
+  import { derived, get, writable } from "svelte/store";
+  import EarnHeader from "../../../components/EarnHeader.svelte";
+  import Offer from "../../../components/Offer.svelte";
+  import OfferPage from "../../../components/OfferPage.svelte";
+  import { createQuery } from "@tanstack/svelte-query";
+  import { page } from "$app/stores";
+  import { browser } from "$app/environment";
+  import LoadingSpinner from "../../../components/common/Loading/LoadingSpinner.svelte";
+  import { offerStore } from "$stores/offer.store";
 
-    var heading;
-    export let data;
-    var userId = "";
-    var offers = [];
-    var filteredOffers = [];
+  let devices = {
+    android: true,
+    apple: true,
+    computer: true,
+  };
 
-    const filterOffers = () => filteredOffers = offers.filter(offer => {
-        if (heading.apple && (offer.mobile_app_type === "ios" || offer.mobile_app_type === null)) {
-            return true;
+  // export let data;
+
+  let userId = "";
+  $: typeName = $page.params.type;
+
+  // $page.subscribe((val) => {
+  //   typeName = val.params.type;
+  // });
+
+  const offers = derived(offerStore, ($offerStore) => {
+    return $offerStore[typeName]?.data || [];
+  });
+
+  let filteredOffers = [];
+  let loading = false;
+
+  const filterOffers = (devices, offers) => {
+    // console.log("ran");
+    filteredOffers =
+      offers?.filter((offer) => {
+        // console.log(offer, "offer");
+        if (
+          devices.apple &&
+          (offer.mobile_app_type === "ios" || offer.mobile_app_type === null)
+        ) {
+          return true;
         }
 
-        if (heading.android && (offer.mobile_app_type === "android" || offer.mobile_app_type === null)) {
-            return true;
+        if (
+          devices.android &&
+          (offer.mobile_app_type === "android" ||
+            offer.mobile_app_type === null)
+        ) {
+          return true;
         }
 
-        if (heading.computer && (offer.mobile_app_type === null)) {
-            return true;
+        if (devices.computer && offer.mobile_app_type === null) {
+          return true;
         }
 
         return false;
+        // empty array is assigned to filtered offer because while its loading the data is undefined
+      }) || [];
+  };
+
+  // console.log(filtered, "filtered");
+
+  // filteredOffers = filtered;
+  // };
+
+  // const fetchOffers = async (typename) => {
+  //   // set offers as empty array , while loading
+  //   loading = true;
+  //   offers = [];
+  //   filterOffers(devices);
+
+  //   const response = await fetch("/api/offers?categoryName=" + typename);
+  //   const responseData = await response.json();
+
+  //   if (!responseData.success) {
+  //     // TODO: handle error
+  //     return;
+  //   }
+
+  //   offers = responseData.offers;
+  //   filterOffers(devices);
+  //   loading = false;
+  // };
+
+  // runs in browser everytime data.typeName changes
+  // to fetch offers based on typeName
+  // $: if (browser) fetchOffers(data.typeName);
+
+  // $: offers = $offerStore[data.typeName]?.data || [];
+
+  $: filterOffers(devices, $offers);
+
+  /**
+   * @param {string[]} str
+   * @returns {string[]}
+   */
+  const capitalizeFirstLetter = (str) => {
+    return str.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+  };
+
+  let limit = 40;
+  let offerPageNode;
+
+  const getOffers = async (page, limit) => {
+    const searchCategory = typeName;
+    // console.log(searchCategory, "searchCategory");
+
+    // set the data for current category to empty so user see loading screen
+    offerStore.set({
+      ...$offerStore,
+      [searchCategory]: {
+        // append to prev offers
+        ...$offerStore[searchCategory],
+        loading: true,
+      },
     });
 
-    onMount(async () => {
-        const response = await fetch("/api/offers?categoryName=" + data.typeName);
-        const responseData = await response.json();
+    const response = await fetch(
+      `/api/offers?page=${page}&limit=${limit}&categoryName=${searchCategory}`
+    );
+    const data = await response.json();
 
-        if (!responseData.success) {
-            // TODO: handle error
-            return;
-        }
+    if (!data.success) {
+      // TODO: handle error
+      return;
+    }
 
-        offers = responseData.offers;
-        filterOffers();
+    offerStore.set({
+      ...$offerStore,
+      [searchCategory]: {
+        // append to prev offers
+        data: [...$offerStore[searchCategory].data, ...data.offers.docs],
+        loading: false,
+        apiResponse: data,
+      },
     });
+
+    filterOffers(devices);
+  };
+
+  onMount(() => {
+    // getOffers(page, limit);
+
+    // to listen to scroll and call api afer user starts reaching end
+    window.addEventListener("scroll", () => {
+      const currentOfferCategory = $offerStore[typeName];
+
+      if (
+        offerPageNode?.scrollHeight - window?.scrollY < 500 &&
+        currentOfferCategory.apiResponse?.offers?.hasNextPage &&
+        currentOfferCategory.loading === false
+      ) {
+        // paginationPage = paginationPage + 1;
+        getOffers(currentOfferCategory.apiResponse?.offers?.nextPage, limit);
+        // console.log("hit api now", page);
+      }
+      // console.log(window?.scrollY, offerPageNode?.scrollHeight);
+    });
+  });
+  // $: console.log($offerStore, data, $offerStore[data.typeName]?.data, "page");
+
+  // $: console.log($offers, "$offers");
+  // $: console.log(devices, "devices");
+  $: console.log($offerStore, "offerstore");
 </script>
 
 <svelte:head>
-    <title>{data.typeName} - Justearn.com</title>
+  <title
+    >{capitalizeFirstLetter(typeName.split("-")).join(" ")} - Justearn.com</title
+  >
 </svelte:head>
 
-<EarnHeader name={data.typeName} bind:this={heading}/>
+<EarnHeader
+  name={capitalizeFirstLetter(typeName.split("-")).join(" ")}
+  bind:devices
+/>
+<!-- typeName= {typeName } filteredOffers length = {filteredOffers.length} -->
 
-<OfferPage>
+{#if filteredOffers?.length === 0 && !$offerStore[typeName]?.loading}
+  <div class="spinner-container">No Offers Currently</div>
+{/if}
+
+<div bind:this={offerPageNode}>
+  <OfferPage>
     {#each filteredOffers as offer}
-        <Offer
-            dollars={Math.floor(offer.tokens / 100)}
-            cents={parseInt(offer.tokens % 100)}
-            offerImage={offer.creative}
-            title={offer.title}
-            description={offer.description}
-            type="OTHER"
-            android={(offer.mobile_app_type === "android" || offer.mobile_app_type === null)}
-            apple={(offer.mobile_app_type === "ios" || offer.mobile_app_type === null)}
-            computer={(offer.mobile_app_type === null)}
-            offerUrl={offer.link.replace("[USERIDHERE]", userId)}
-        />
+      <Offer
+        currencyAward={offer.tokens}
+        offerImage={offer.creative}
+        title={offer.title}
+        description={offer.description}
+        type={typeName}
+        android={offer.mobile_app_type === "android" ||
+          offer.mobile_app_type === null}
+        apple={offer.mobile_app_type === "ios" ||
+          offer.mobile_app_type === null}
+        computer={offer.mobile_app_type === null}
+        offerUrl={offer.link.replace("[USERIDHERE]", userId)}
+      />
     {/each}
-</OfferPage>
+  </OfferPage>
+</div>
 
+{#if $offerStore[typeName]?.loading}
+  <div class="spinner-container">
+    <LoadingSpinner />
+  </div>
+{/if}
+
+<style lang="scss">
+  .spinner-container {
+    display: grid;
+    place-items: center;
+    height: 100px;
+    width: 100%;
+    background: #171515;
+  }
+</style>
