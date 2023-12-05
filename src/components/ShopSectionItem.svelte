7d8fc2cb4ab4d193bc05db07fa247a1dc6820587
beanznotavailable <59755055+beanznotavailable@users.noreply.github.com>
2023-12-05 11:00:53 +0545

    fix: scaling issue of payoutmethod in mozilla browser

--- src/components/ShopSectionItem.svelte
+++ src/components/ShopSectionItem.svelte
@@ -1,37 +1,41 @@
 <script>
-    export let url = "";
-    export let background = "";
-    export let imageUrl = "";
-    export let imageWidth = false;
+  export let url = "";
+  export let background = "";
+  export let imageUrl = "";
+  export let imageWidth = false;
 </script>
 
 <a href={"/cashout/" + url} style="background: {background}">
-    <img src={imageUrl} alt="logo" style={imageWidth ? "width: " + imageWidth.toString() + "px" : ""}/>
+  <img
+    src={imageUrl}
+    alt="logo"
+    style={imageWidth ? "width: " + imageWidth.toString() + "px" : ""}
+  />
 </a>
 
 <style lang="scss">
+  a {
+    display: grid;
+    place-items: center;
+    width: 212px;
+    height: 102px;
+    border-radius: 5px;
+    box-shadow: 0 10px 15px rgb(0 0 0 / 15%);
+    margin-right: 10px;
+    margin-bottom: 10px;
+  }
+
+  img {
+    height: 50%;
+    width: 50%;
+    aspect-ratio: 1;
+    object-fit: contain;
+  }
+
+  @media only screen and (max-width: 700px) {
     a {
-        display: block;
-        position: relative;
-        width: 212px;
-        height: 102px;
-        border-radius: 5px;
-        box-shadow: 0 10px 15px rgb(0 0 0 / 15%);
-        margin-right: 10px;
-        margin-bottom: 10px;
+      width: 47%;
+      height: auto;
     }
-
-    img {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%,-50%);
-        max-width: -webkit-fill-available;
-    }
-
-    @media only screen and (max-width: 700px) {
-        a {
-            width: 47%;
-        }
-    }
+  }
 </style>
