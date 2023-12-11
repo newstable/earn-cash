d8c70f48d2908efc92b5098487d27e84c3191d97
beanznotavailable <59755055+beanznotavailable@users.noreply.github.com>
2023-12-11 19:00:16 +0545

    fix: user holds not working

--- src/routes/admin/user/[userid]/holds/+page.server.js
+++ src/routes/admin/user/[userid]/holds/+page.server.js
@@ -1,12 +1,17 @@
+import { json } from "@sveltejs/kit";
 import Reward from "../../../../../models/Reward.model.js";
 import User from "../../../../../models/User.model.js";
 
 export const load = async (request) => {
   const userid = request.params.userid;
+  // console.log(userid);
+
   const user = await User.findById(userid);
 
   const getHolds = await Reward.find({ user }).sort({ date: -1 });
+
+  // console.log(getHolds, "getHOld");
   return {
-    holds: getHolds || [],
+    holds: JSON.parse(JSON.stringify(getHolds)),
   };
 };
