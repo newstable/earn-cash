a90e21f959c4cc1fd16549c84d2ce54e78c11ec4
beanznotavailable <59755055+beanznotavailable@users.noreply.github.com>
2023-12-13 15:00:45 +0545

    update for ip and country being null

--- src/routes/api/user/authenticate/+server.js
+++ src/routes/api/user/authenticate/+server.js
@@ -1,76 +1,87 @@
 import response from "$lib/response";
 import mustBeHere from "$lib/server/mustBeHere";
-import validation from '$lib/validation';
+import validation from "$lib/validation";
 import doLogin from "../../../../lib/server/doLogin";
 import { checkPassword } from "../../../../lib/server/passwords";
 import userRequest from "../../../../lib/server/userRequest";
 import User from "../../../../models/User.model";
 
-export const GET = async(request) => {
-    const user = await request.getAuthenticatedUser();
-    const geoInfo = {
-        ip: request.request.headers.get("ip"),
-        country: request.request.headers.get("country")
-    }
+export const GET = async (request) => {
+  const user = await request.getAuthenticatedUser();
+  const geoInfo = {
+    ip: request.request.headers.get("cf-connecting-ip"),
+    country: request.request.headers.get("cf-ipcountry"),
+  };
+  // const geoInfo = {
+  //     ip: request.request.headers.get("ip"),
+  //     country: request.request.headers.get("country")
+  // }
 
-    if (user === false) {
-        return response({
-            success: false
-        });
-    }
-
-    userRequest(user, geoInfo);
-
+  if (user === false) {
     return response({
-        success: true,
-        user: {
-            username: user.username,
-            balance: user.points - user.cashedOut - user.removedPoints + user.addedPoints,
-            picture: user.picture,
-            id: user._id
-        }
+      success: false,
     });
-}
+  }
 
-export const POST = async(request) => {
-    var data;
+  userRequest(user, geoInfo);
 
-    try {
-        data = await request.request.json();
-    } catch (e) {
-        return response({
-            success: false
-        }, 400);
-    }
+  return response({
+    success: true,
+    user: {
+      username: user.username,
+      balance:
+        user.points - user.cashedOut - user.removedPoints + user.addedPoints,
+      picture: user.picture,
+      id: user._id,
+    },
+  });
+};
 
-    if (mustBeHere(data.email)) return response({ success: false }, 400);
-    if (mustBeHere(data.password)) return response({ success: false }, 400);
+export const POST = async (request) => {
+  var data;
 
-    if (!validation.email(data.email)) return response({ success: false }, 406);
-    if (!validation.password(data.password)) return response({ success: false }, 406);
-
-    if (await request.isAuthenticated()) return response({ success: false }, 406);
-
-    const user = await User.findOne({
-        email: data.email.toLowerCase()
-    });
-    if (user === null) return response({
+  try {
+    data = await request.request.json();
+  } catch (e) {
+    return response(
+      {
         success: false,
-        message: "EP not OK",
-        userMessage: "Email or password incorrect"
-    });
+      },
+      400
+    );
+  }
 
-    const passwordResult = await checkPassword(user.password, data.password);
-    if (!passwordResult) return response({
-        success: false,
-        message: "EP not 0K",
-        userMessage: "Email or password incorrect"
-    });
+  if (mustBeHere(data.email)) return response({ success: false }, 400);
+  if (mustBeHere(data.password)) return response({ success: false }, 400);
 
-    userRequest(user, data);
+  if (!validation.email(data.email)) return response({ success: false }, 406);
+  if (!validation.password(data.password))
+    return response({ success: false }, 406);
 
+  if (await request.isAuthenticated()) return response({ success: false }, 406);
+
+  const user = await User.findOne({
+    email: data.email.toLowerCase(),
+  });
+  if (user === null)
     return response({
-        success: true,
-        token: doLogin(user._id)
+      success: false,
+      message: "EP not OK",
+      userMessage: "Email or password incorrect",
     });
-}
+
+  const passwordResult = await checkPassword(user.password, data.password);
+  if (!passwordResult)
+    return response({
+      success: false,
+      message: "EP not 0K",
+      userMessage: "Email or password incorrect",
+    });
+
+  userRequest(user, data);
+
+  return response({
+    success: true,
+    token: doLogin(user._id),
+  });
+};
--- src/routes/api/user/details/+server.js
+++ src/routes/api/user/details/+server.js
@@ -1,69 +1,83 @@
 import response from "$lib/response";
 import mustBeHere from "$lib/server/mustBeHere";
-import validation from '$lib/validation';
+import validation from "$lib/validation";
 import doLogin from "../../../../lib/server/doLogin";
 import { checkPassword } from "../../../../lib/server/passwords";
 import userRequest from "../../../../lib/server/userRequest";
 import User from "../../../../models/User.model";
 
 export const GET = async (request) => {
-    const user = await request.getAuthenticatedUser();
-    const geoInfo = {
-        ip: request.request.headers.get("ip"),
-        country: request.request.headers.get("country")
-    }
+  const user = await request.getAuthenticatedUser();
+  const geoInfo = {
+    // ip: request.request.headers.get("ip"),
+    // country: request.request.headers.get("country")
 
-    if (user === false) {
-        return response({
-            error: "Authentication failed"
-        });
-    }
+    ip: request.request.headers.get("cf-connecting-ip"),
+    country: request.request.headers.get("cf-ipcountry"),
+  };
 
-    userRequest(user, geoInfo);
-
+  if (user === false) {
     return response({
-        username: user.username,
-        balance: user.points - user.cashedOut - user.removedPoints + user.addedPoints,
-        picture: user.picture,
-        id: user._id,
-        level: user.level
+      error: "Authentication failed",
     });
-}
+  }
+
+  userRequest(user, geoInfo);
+
+  return response({
+    username: user.username,
+    balance:
+      user.points - user.cashedOut - user.removedPoints + user.addedPoints,
+    picture: user.picture,
+    id: user._id,
+    level: user.level,
+  });
+};
 
 export const POST = async (request) => {
-    var data;
+  var data;
 
-    try {
-        data = await request.request.json();
-    } catch (e) {
-        return response({
-            error: "Invalid request data"
-        }, 400);
-    }
+  try {
+    data = await request.request.json();
+  } catch (e) {
+    return response(
+      {
+        error: "Invalid request data",
+      },
+      400
+    );
+  }
 
-    if (mustBeHere(data.email)) return response({ error: "Email is required" }, 400);
-    if (mustBeHere(data.password)) return response({ error: "Password is required" }, 400);
+  if (mustBeHere(data.email))
+    return response({ error: "Email is required" }, 400);
+  if (mustBeHere(data.password))
+    return response({ error: "Password is required" }, 400);
 
-    if (!validation.email(data.email)) return response({ error: "Invalid email format" }, 406);
-    if (!validation.password(data.password)) return response({ error: "Invalid password format" }, 406);
+  if (!validation.email(data.email))
+    return response({ error: "Invalid email format" }, 406);
+  if (!validation.password(data.password))
+    return response({ error: "Invalid password format" }, 406);
 
-    if (await request.isAuthenticated()) return response({ error: "Already authenticated" }, 406);
+  if (await request.isAuthenticated())
+    return response({ error: "Already authenticated" }, 406);
 
-    const user = await User.findOne({
-        email: data.email.toLowerCase()
-    });
-    if (user === null) return response({
-        error: "Email or password incorrect"
-    });
-
-    const passwordResult = await checkPassword(user.password, data.password);
-    if (!passwordResult) return response({
-        error: "Email or password incorrect"
-    });
-
-    userRequest(user, data);
-
+  const user = await User.findOne({
+    email: data.email.toLowerCase(),
+  });
+  if (user === null)
     return response({
-        token: doLogin(user._id)
+      error: "Email or password incorrect",
     });
-}
+
+  const passwordResult = await checkPassword(user.password, data.password);
+  if (!passwordResult)
+    return response({
+      error: "Email or password incorrect",
+    });
+
+  userRequest(user, data);
+
+  return response({
+    token: doLogin(user._id),
+  });
+};
