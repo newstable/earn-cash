5c16878ec7978be2c294dd99f40ab62fd1b96b83
beanznotavailable <59755055+beanznotavailable@users.noreply.github.com>
2024-02-09 13:17:20 +0545

    fix: content & footer vertical gap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,39 @@
 <script>
-    import "../app.scss";
-    import Content from "../components/Home/Content.svelte";
-    import Heading from "../components/Home/Heading.svelte";
+  import "../app.scss";
+  import Content from "../components/Home/Content.svelte";
+  import Heading from "../components/Home/Heading.svelte";
 
-    export var data;
+  export var data;
 </script>
 
 <svelte:head>
-    <title>Earn Cash & R$ | justearn.gg </title>
+  <title>Earn Cash & R$ | justearn.gg</title>
 </svelte:head>
 
 <div class="home">
-    <Heading count={data.count} highest={data.highest}/>
-    <Content totalEarned={data.totalEarned}/>
+  <Heading count={data.count} highest={data.highest} />
+  <Content totalEarned={data.totalEarned} />
 </div>
 
 <style lang="scss">
-    div.home {
-        position: relative;
-        letter-spacing: -0.03em;
-        padding-top: 57px;
-        margin: 0 auto 67px auto;
-        background-color: #0E0C1D;
-        
+  div.home {
+    position: relative;
+    letter-spacing: -0.03em;
+    padding-top: 57px;
+    // margin: 0 auto 67px auto;
+    margin: 0 auto 0px auto;
+    background-color: #0e0c1d;
 
-        &::before {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100vh;
-            /* background-image: url(/coin.svg); TODO: give different image */
-            background-repeat: no-repeat;
-            z-index: -1;
-            
-        }
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100vh;
+      /* background-image: url(/coin.svg); TODO: give different image */
+      background-repeat: no-repeat;
+      z-index: -1;
     }
+  }
 </style>
